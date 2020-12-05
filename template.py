45a1a33da309efdf4974bdf19c72010b31f9311b
Daniel Jackoway <daniel@quip.com>
2020-12-05 15:51:29 -0500

    make a template file

---
+++ template.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import fileinput
+
+
+def part1():
+    [line.strip() for line in fileinput.input()]
+    for line in fileinput.input():
+        pass
+    return
+
+def part2():
+    [line.strip() for line in fileinput.input()]
+    for line in fileinput.input():
+        pass
+    return
+
+part1()
