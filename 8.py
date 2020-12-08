8f122c0aa8579071103681827c6f4d55ec20253a
Daniel Jackoway <daniel@quip.com>
2020-12-08 00:16:30 -0500

    brute force 8 part2 was fastest to write

--- 8.py
+++ 8.py
@@ -27,10 +27,40 @@ def part1():
             idx += 1
     return acc
 
-def part2():
-    [line.strip() for line in fileinput.input()]
-    for line in fileinput.input():
-        pass
-    return
+def does_terminate(instructions):
+    acc = 0
+    idx = 0
+    seen_idx = set()
+    while idx < len(instructions):
+        if idx in seen_idx:
+            return False, acc
+        seen_idx.add(idx)
+        ins, val = parse_line(instructions[idx])
+        if ins == "acc":
+            acc += val
+            idx += 1
+        elif ins == "jmp":
+            idx += val
+        else:
+            assert ins == "nop"
+            idx += 1
+    return idx == len(instructions), acc
 
-print(part1())
+
+def part2():
+    instructions = [line.strip() for line in fileinput.input()]
+    for i in range(len(instructions)):
+        ins, val = parse_line(instructions[i])
+        if ins == "acc":
+            continue
+        new = instructions.copy()
+        if ins == "nop":
+            new[i] = " ".join(["jmp", str(val)])
+        else:
+            new[i] = " ".join(["nop", str(val)])
+        terminates, acc = does_terminate(new)
+        if terminates:
+            return acc
+
+#print(part1())
+print(part2())
