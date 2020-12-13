cb92be448748a515a2bfce4e8b13efa260c7c2d2
Daniel Jackoway <daniel@quip.com>
2020-12-12 23:58:59 -0500

    13 from template

---
+++ 13.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import fileinput
+import re
+
+
+def part1():
+    [line.strip() for line in fileinput.input()]
+    for line in fileinput.input():
+        pass
+    return
+
+def part2():
+    [line.strip() for line in fileinput.input()]
+    for line in fileinput.input():
+        pass
+    return
+
+print(part1())
+#print(part2())
