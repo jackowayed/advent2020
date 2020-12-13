eefca18b8a01d642e55e5e2e28b331d1223473be
Daniel Jackoway <daniel@quip.com>
2020-12-13 00:07:07 -0500

    part1

--- 13.py
+++ 13.py
@@ -5,10 +5,17 @@ import re
 
 
 def part1():
-    [line.strip() for line in fileinput.input()]
-    for line in fileinput.input():
-        pass
-    return
+    lines = [line.strip() for line in fileinput.input()]
+    start_time = int(lines[0])
+    buses = [int(l) for l in lines[1].split(",") if not l.startswith("x")]
+    min_ = 100000
+    min_freq = None
+    for freq in buses:
+        wait = (freq - (start_time % freq)) % freq
+        if wait < min_:
+            min_ = wait
+            min_freq = freq
+    return min_freq * min_
 
 def part2():
     [line.strip() for line in fileinput.input()]
