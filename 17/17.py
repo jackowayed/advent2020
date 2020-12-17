4248ccc7360035c44c243b870705a20236a9b17b
Daniel Jackoway <daniel@quip.com>
2020-12-17 00:20:50 -0500

    17.2

--- 17/17.py
+++ 17/17.py
@@ -4,40 +4,31 @@ import collections
 import fileinput
 import re
 
-class Coordinate__:
-    def __init__(self, x, y, z):
-        self.x = x
-        self.y = y
-        self.z = z
-
-    def __add__(self, c):
-        return Coordinate__(c.x + self.x, 
-            c.y + self.y,
-            c.z + self.z)
-
-Coordinate = collections.namedtuple("Coordinate", ["x", "y", "z"])
+Coordinate = collections.namedtuple("Coordinate", ["x", "y", "z", "a"])
 
 def neighbors(c, m):
     ct = 0
-    x, y, z = c
+    x, y, z, a = c
     for dx in range(-1, 2):
         for dy in range(-1, 2):
             for dz in range(-1, 2):
-                if dx == 0 and dy == 0 and dz == 0:
-                    continue
-                ct += (x + dx, y + dy, z + dz) in m
+                for da in range(-1, 2):
+                    if dx == 0 and dy == 0 and dz == 0 == da:
+                        continue
+                    ct += (x + dx, y + dy, z + dz, a + da) in m
     return ct
 
 
 def do_turn(m):
     new_m = set()
     candidates = set()
-    for x,y,z in m:
+    for x,y,z,a in m:
         for dx in range(-1, 2):
             for dy in range(-1, 2):
                 for dz in range(-1, 2):
-                    candidates.add(Coordinate(
-                        x + dx, y + dy, z + dz))
+                    for da in range(-1, 2):
+                        candidates.add(Coordinate(
+                            x + dx, y + dy, z + dz, a + da))
     for c in candidates:
         n = neighbors(c, m)
         if n == 3 or (n == 2 and c in m):
@@ -51,7 +42,7 @@ def part1():
     for y, line in enumerate(start_map):
         for x, char in enumerate(line):
             if char == "#":
-                m.add(Coordinate(x, y, 0))
+                m.add(Coordinate(x, y, 0, 0))
             else:
                 assert char == "."
     
