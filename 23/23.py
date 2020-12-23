10e1bb9c76e45fa5268cafdcdbe807f1d41b3fa6
Daniel Jackoway <daniel@quip.com>
2020-12-23 14:30:11 -0500

    23 p2

--- 23/23.py
+++ 23/23.py
@@ -9,63 +9,58 @@ def rotate_to_front(l, val):
         l.insert(0, l.pop())
 
 def part1():
-    #input = "962713854"
-    input = "389125467"
+    N_CUPS = 9
+    input = "962713854"
+    #input = "389125467"
     cl = CircularList()
     for c in input:
         cl.insert_at_tail(int(c))
-    cl.print_list()
+    print(len(cl))
     solve(cl, 100)
-    cl.print_list()
-
-def solve_p1(cups, turns):
-    n_cups = len(cups)
-    for turn in range(turns):
-        if turn % 1000 == 0:
-            print(turn)
-        start = cups[0]
-        pickup = cups[1:4]
-        cups = cups[:1] + cups[4:]
-        dest_idx = -1
-        for n in range(cups[0] - 1, -1, -1):
-            if n in cups:
-                dest_idx = cups.index(n)
-                break
-        if dest_idx == -1:
-            max_ = max(cups)
-            dest_idx = cups.index(max_)
-        old_cups = cups
-        cups = cups[:dest_idx + 1] + pickup + cups[dest_idx + 1:]
-        assert len(cups) == n_cups
-        #    cups = old_cups[:len(cups) - n_cups] + cups
-        #    print(cups)
-        next_idx = (cups.index(start) + 1) % len(cups)
-        rotate_to_front(cups, cups[next_idx])
-    rotate_to_front(cups, 1)
-    return cups
-
+    print(cl)
 
 class CupNode:
     def __init__(self, val):
         self.val = val
         self.next = self.prev = None
 
+    def __repr__(self):
+        return f"{self.val} (p {self.prev.val} n {self.next.val})"
+
+    def __str__(self):
+        return self.__repr__()
+
+
 class CircularList:
     def __init__(self):
         self.head = None
         self.index = dict()
 
-    def print_list(self):
+    def __repr__(self):
         s = str(self.head.val)
         ct = 0
         node = self.head.next
-        while node.val != self.head.val:
-            s += str(node.val)
+        while node != self.head:
+            s += "," + str(node.val)
             node = node.next
             ct += 1
             if ct > 20:
                 break
-        print(s)
+        return s
+
+    def __str__(self):
+        return self.__repr__()
+
+    def __len__(self):
+        if not self.head:
+            return 0
+        ct = 1
+        node = self.head.next
+        while node != self.head:
+            node = node.next
+            ct += 1
+        return ct
+
 
     def insert_at_tail(self, val):
         if not self.head:
@@ -80,9 +75,10 @@ class CircularList:
         node.next.prev = new_node
         node.next = new_node
         new_node.prev = node
+        assert node.next != node, node.val
         #if new_node.val in self.index and self.index[new_node.val] != new_node:
             #assert False, f"{new_node.val} {"
-        self.index[new_node.val] = node
+        self.index[new_node.val] = new_node
 
     def remove_after(self,node):
         rem = node.next
@@ -95,12 +91,14 @@ N_CUPS = 1000000
 
 def next_value(cur_val, removed_vals):
     next_val = cur_val - 1
+    if next_val == 0:
+        next_val = N_CUPS
     while next_val in removed_vals:
         next_val -= 1
-    if next_val == 0:
-        return N_CUPS
-    else:
-        return next_val
+        if next_val == 0:
+            next_val = N_CUPS
+    assert next_val > 0
+    return next_val
 
 def solve(cl, turns):
     for turn in range(turns):
@@ -117,16 +115,18 @@ def solve(cl, turns):
 
 
 def part2():
-    #input = "962713854" 
-    input = "389125467"  # test
+    input = "962713854"
+    #input = "389125467"  # test
     cl = CircularList()
     for c in input:
         cl.insert_at_tail(int(c))
-    for i in range(len(input), N_CUPS):
+    for i in range(len(input) + 1, N_CUPS + 1):
         cl.insert_at_tail(i)
+    print(len(cl))
     solve(cl, 10000000)
+    print(cl)
     print((cl.head.next.val, cl.head.next.next.val))
     return cl.head.next.val * cl.head.next.next.val
 
-print(part1())
-#print(part2())
+#print(part1())
+print(part2())
